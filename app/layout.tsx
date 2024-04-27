7174da820dafd68ea141cc139adc95b7278e3e76
hoanggbao00 <kaitou.kurosaki@gmail.com>
2024-04-27 19:50:42 +0700

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,47 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '@/styles/globals.css';
-import '@/styles/prosemirror.css';
-import Header from '@/components/header/header';
-import Providers from '@/providers/providers';
-import { Toaster } from '@/components/ui/toaster';
-import NextTopLoader from 'nextjs-toploader';
-import { cn } from '@/lib/utils';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+import "@/styles/prosemirror.css";
+import Header from "@/components/header/header";
+import Providers from "@/providers/providers";
+import { Toaster } from "@/components/ui/toaster";
+import NextTopLoader from "nextjs-toploader";
+import { cn } from "@/lib/utils";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: 'EAUT Community',
-	description: 'QnA Community of EAUT',
+  title: "EAUT Community",
+  description: "QnA Community of EAUT",
 };
 
 export default function RootLayout({
-	children,
-	authModal,
+  children,
+  authModal,
 }: Readonly<{
-	children: React.ReactNode;
-	authModal: React.ReactNode;
+  children: React.ReactNode;
+  authModal: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body
-				className={cn(
-					'min-h-screen bg-background text-foreground antialiased',
-					inter.className
-				)}
-			>
-				<NextTopLoader />
-				<Providers>
-					<Header />
-					{authModal}
-					<main className='mx-auto h-full px-2 sm:px-2 pt-20 sm:container xl:pl-72'>
-						{children}
-					</main>
-					<Toaster />
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background text-foreground antialiased",
+          inter.className,
+        )}
+      >
+        <NextTopLoader />
+        <Providers>
+          <Header />
+          {authModal}
+          <main className="mx-auto h-full px-2 pt-20 sm:container sm:px-2 xl:pl-72">
+            {children}
+          </main>
+          <Toaster />
+        </Providers>
+        <SpeedInsights />
+      </body>
+    </html>
+  );
 }
