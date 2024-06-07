ee72b29ccfcc70bea656e7ea14c33493829055f5
hoanggbao00 <78465334+hoanggbao00@users.noreply.github.com>
2024-06-07 15:32:10 +0700

    Update next.config.mjs
    change cors

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
 					// Allow for specific domains to have access or * for all
 					{
 						key: 'Access-Control-Allow-Origin',
-						value: '*',
+						value: 'https://eaut-community.vercel.app/',
 						// DOES NOT WORK
 						// value: process.env.ALLOWED_ORIGIN,
 					},
